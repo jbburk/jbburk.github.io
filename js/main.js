adfeb341309f68f80df71c9da4603a27c13ccbb8
jbburk <jbburk@byui.edu>
2021-04-24 13:47:14 -0500

    4/24 1:50p commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-}
-
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
-
 // set current year in footer
 const currentDate = new Date();
-document.querySelector('#year').textContent = currentDate.getFullYear();
+document.querySelector("#year").textContent = currentDate.getFullYear();
+
+// set date last modified in footer
+document.querySelector("#lastmod").textContent = document.lastModified;
