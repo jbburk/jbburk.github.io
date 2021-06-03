bd0843cb027ef2f128d333cdc26c1e85b7106602
jbburk <jbburk@byui.edu>
2021-06-03 12:58:43 -0500

    progressive image loading

---
+++ lesson7/js/imagehandling.js
@@ -0,0 +1,33 @@
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const options = {
+    rootMargin: '0px 0px -200px 0px',
+    threshold: 0
+};
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items) => {
+        items.forEach((item)=> {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, options);
+
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
