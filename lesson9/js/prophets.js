ac6a093eaa687c78a8a4e891ea45a14cd2d3634c
jbburk <jbburk@byui.edu>
2021-06-17 11:57:42 -0500

    fixed image

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -7,7 +7,7 @@ fetch(requestURL)
     .then(function (jsonObject) {
         const prophets = jsonObject['prophets'];
         console.table(jsonObject); // temporary checking for valid response and data parsing
-        for (let i = 0; i < prophets.length; i++ ) {
+        for (let i = 0; i < prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let birthDate = document.createElement('p');
@@ -17,7 +17,7 @@ fetch(requestURL)
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
             birthDate.textContent = "Date of Birth: " + prophets[i].birthdate;
             birthPlace.textContent = "Place of Birth: " + prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageur1);
+            image.setAttribute('src', prophets[i].imageurl);
             image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + " - " + i);
 
             card.appendChild(h2);
